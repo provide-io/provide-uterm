ec766226133ff4a6c7c07357675a12217638d016
Tim <code@tim.life>
2026-07-20 21:25:47 -0700

    Fix VNC input authorization and headless RFB client

--- packages/provide-uterm-go/vnc/litevirt_ai.go
+++ packages/provide-uterm-go/vnc/litevirt_ai.go
@@ -106,7 +106,16 @@ func (c *LitevirtAIClient) RunHandshakeAndLoop() error {
 		return err
 	}
 	if numSecTypes[0] == 0 {
-		return fmt.Errorf("connection failed")
+		var reasonLen [4]byte
+		if _, err := io.ReadFull(r, reasonLen[:]); err != nil {
+			return fmt.Errorf("connection failed, could not read reason length")
+		}
+		l := binary.BigEndian.Uint32(reasonLen[:])
+		reason := make([]byte, l)
+		if _, err := io.ReadFull(r, reason); err != nil {
+			return fmt.Errorf("connection failed, could not read reason string")
+		}
+		return fmt.Errorf("connection failed: %s", string(reason))
 	}
 
 	secTypes := make([]byte, numSecTypes[0])
@@ -114,7 +123,17 @@ func (c *LitevirtAIClient) RunHandshakeAndLoop() error {
 		return err
 	}
 
-	// Assume type 1 (None) is supported
+	hasNone := false
+	for _, st := range secTypes {
+		if st == 1 {
+			hasNone = true
+			break
+		}
+	}
+	if !hasNone {
+		return fmt.Errorf("security type None not supported by server")
+	}
+
 	c.streamMu.Lock()
 	err = c.stream.Send(&pb.VNCData{Data: []byte{1}})
 	c.streamMu.Unlock()
@@ -158,8 +177,46 @@ func (c *LitevirtAIClient) RunHandshakeAndLoop() error {
 	c.trackerMu.Unlock()
 
 	// 5. SetPixelFormat (RGBA 32-bit) & SetEncodings (Raw = 0)
-	// We'll skip sending the literal packets here for brevity but in reality we'd construct and send them.
-	// For this test task, we assume the server defaults to Raw or we rely on QEMU defaults.
+	pfMsg := make([]byte, 20)
+	pfMsg[0] = 0 // ClientSetPixelFormat
+	pfMsg[4] = 32 // bits-per-pixel
+	pfMsg[5] = 24 // depth
+	pfMsg[6] = 1  // big-endian flag
+	pfMsg[7] = 1  // true-colour flag
+	binary.BigEndian.PutUint16(pfMsg[8:10], 255)  // red-max
+	binary.BigEndian.PutUint16(pfMsg[10:12], 255) // green-max
+	binary.BigEndian.PutUint16(pfMsg[12:14], 255) // blue-max
+	pfMsg[14] = 24 // red-shift
+	pfMsg[15] = 16 // green-shift
+	pfMsg[16] = 8  // blue-shift
+
+	encMsg := make([]byte, 8)
+	encMsg[0] = 2 // ClientSetEncodings
+	binary.BigEndian.PutUint16(encMsg[2:4], 1) // 1 encoding
+	binary.BigEndian.PutUint32(encMsg[4:8], 0) // Raw = 0
+
+	c.streamMu.Lock()
+	err = c.stream.Send(&pb.VNCData{Data: append(pfMsg, encMsg...)})
+	c.streamMu.Unlock()
+	if err != nil {
+		return err
+	}
+
+	// Request initial full update
+	reqMsg := make([]byte, 10)
+	reqMsg[0] = 3 // ClientFramebufferUpdateRequest
+	reqMsg[1] = 0 // incremental=0
+	binary.BigEndian.PutUint16(reqMsg[2:4], 0)
+	binary.BigEndian.PutUint16(reqMsg[4:6], 0)
+	binary.BigEndian.PutUint16(reqMsg[6:8], width)
+	binary.BigEndian.PutUint16(reqMsg[8:10], height)
+
+	c.streamMu.Lock()
+	err = c.stream.Send(&pb.VNCData{Data: reqMsg})
+	c.streamMu.Unlock()
+	if err != nil {
+		return err
+	}
 
 	// 6. FramebufferUpdate loop
 	for {
@@ -167,46 +224,78 @@ func (c *LitevirtAIClient) RunHandshakeAndLoop() error {
 		if _, err := io.ReadFull(r, msgType[:]); err != nil {
 			return err
 		}
-		if msgType[0] != ServerFramebufferUpdate {
+
+		switch msgType[0] {
+		case ServerFramebufferUpdate: // 0
+			var header [3]byte // padding + numRects
+			if _, err := io.ReadFull(r, header[:]); err != nil {
+				return err
+			}
+			numRects := binary.BigEndian.Uint16(header[1:3])
+
+			for i := 0; i < int(numRects); i++ {
+				var rect [12]byte // x, y, w, h, encoding
+				if _, err := io.ReadFull(r, rect[:]); err != nil {
+					return err
+				}
+				rx := int(binary.BigEndian.Uint16(rect[0:2]))
+				ry := int(binary.BigEndian.Uint16(rect[2:4]))
+				rw := int(binary.BigEndian.Uint16(rect[4:6]))
+				rh := int(binary.BigEndian.Uint16(rect[6:8]))
+				encoding := binary.BigEndian.Uint32(rect[8:12])
+
+				if encoding != 0 {
+					return fmt.Errorf("unsupported encoding: %d", encoding)
+				}
+
+				if rw == 0 || rh == 0 || rx+rw > int(width) || ry+rh > int(height) {
+					return fmt.Errorf("invalid rectangle bounds")
+				}
+
+				pixels := make([]byte, rw*rh*4)
+				if _, err := io.ReadFull(r, pixels); err != nil {
+					return err
+				}
+
+				c.trackerMu.Lock()
+				t := c.tracker
+				c.trackerMu.Unlock()
+				if t != nil {
+					_ = t.ApplyRawUpdate(rx, ry, rw, rh, pixels)
+				}
+			}
+
+			// Request next incremental update
+			reqMsg[1] = 1 // incremental=1
+			c.streamMu.Lock()
+			_ = c.stream.Send(&pb.VNCData{Data: reqMsg})
+			c.streamMu.Unlock()
+
+		case 1: // ServerSetColourMapEntries
+			var header [5]byte // padding + first + number
+			if _, err := io.ReadFull(r, header[:]); err != nil {
+				return err
+			}
+			num := binary.BigEndian.Uint16(header[3:5])
+			if _, err := io.CopyN(io.Discard, r, int64(num)*6); err != nil {
+				return err
+			}
+
+		case 2: // ServerBell
+			// skip
+
+		case 3: // ServerServerCutText
+			var header [7]byte // padding(3) + length(4)
+			if _, err := io.ReadFull(r, header[:]); err != nil {
+				return err
+			}
+			length := binary.BigEndian.Uint32(header[3:7])
+			if _, err := io.CopyN(io.Discard, r, int64(length)); err != nil {
+				return err
+			}
+
+		default:
 			return fmt.Errorf("unsupported server message type: %d", msgType[0])
-		}
-
-		var header [3]byte // padding + numRects
-		if _, err := io.ReadFull(r, header[:]); err != nil {
-			return err
-		}
-		numRects := binary.BigEndian.Uint16(header[1:3])
-
-		for i := 0; i < int(numRects); i++ {
-			var rect [12]byte // x, y, w, h, encoding
-			if _, err := io.ReadFull(r, rect[:]); err != nil {
-				return err
-			}
-			rx := int(binary.BigEndian.Uint16(rect[0:2]))
-			ry := int(binary.BigEndian.Uint16(rect[2:4]))
-			rw := int(binary.BigEndian.Uint16(rect[4:6]))
-			rh := int(binary.BigEndian.Uint16(rect[6:8]))
-			encoding := binary.BigEndian.Uint32(rect[8:12])
-
-			if encoding != 0 {
-				return fmt.Errorf("unsupported encoding: %d", encoding)
-			}
-
-			if rw > int(width) || rh > int(height) {
-				return fmt.Errorf("rectangle too large")
-			}
-
-			pixels := make([]byte, rw*rh*4)
-			if _, err := io.ReadFull(r, pixels); err != nil {
-				return err
-			}
-
-			c.trackerMu.Lock()
-			t := c.tracker
-			c.trackerMu.Unlock()
-			if t != nil {
-				_ = t.ApplyRawUpdate(rx, ry, rw, rh, pixels)
-			}
 		}
 	}
 }
